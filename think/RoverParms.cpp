fe6c751f84da3f02d9e8ba8edc5f25c0921303a3
Haoyang Wang <haoyang@compoundeye.com>
2017-02-16 17:51:52 -0800

    read and write as cv::Mat

--- think/RoverParms.cpp
+++ think/RoverParms.cpp
@@ -2,18 +2,10 @@
 #include <opencv2/core/persistence.hpp>
 
 
-static void ReadMatrix(double* m, int count, const cv::FileNode& node) {
-    for (int i = 0; i < count; ++i) {
-        m[i] = node[i];
-    }
-}
-
 void RoverParms::Read(const std::string& filepath) {
     cv::FileStorage file(filepath, cv::FileStorage::READ);
     if (file.isOpened()) {
-        double perspective_[3*3];
-        ReadMatrix(perspective_, 3*3, file["perspective"]);
-        perspective = cv::Mat_<double>(3, 3, perspective_);
+        perspective = file["perspective"].mat();
     }
 }
 
